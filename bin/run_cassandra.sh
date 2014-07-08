b0af0f5918fb0620f04b2f30b143670b149ace81
Yulian Slobodyan <y.slobodian@gmail.com>
2014-07-08 13:30:33 -0500

    Parameterized cassandra native ports (to be proxied over marathon dynamic proxy), cluster name.

--- bin/run_cassandra.sh
+++ bin/run_cassandra.sh
@@ -5,5 +5,6 @@
 #
 
 sed -e "s/<ZK_HOSTS>/$1/" -e "s/<CASSANDRA_NODES_COUNT>/$2/"  -e "s/<CPUS_RES_REQ>/$3/" -e "s/<MEM_RES_REQ>/$4/" -e "s/<DISK_RES_REQ>/$5/" ./cassandra-mesos-2.0.9-2/conf/mesos.yaml.tmpl > ./cassandra-mesos-2.0.9-2/conf/mesos.yaml
+sed -e "s/<PORT>/$6/"  -e "s/<CLUSTER_NAME>/$7/" ./cassandra-mesos-2.0.9-2/conf/cassandra.yaml.tmpl > ./cassandra-mesos-2.0.9-2/conf/cassandra.yaml
 cd cassandra-mesos-2.0.9-2
 ./bin/cassandra-mesos
