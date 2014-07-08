cb8b41c07a7fa3436fad2fb4bab319ebe88f3764
Yulian Slobodyan <y.slobodian@gmail.com>
2014-07-08 18:08:39 -0500

    Send HTTP 200 for "/" request to config server - use for health monitoring

--- src/main/scala/mesosphere/cassandra/ConfigServer.scala
+++ src/main/scala/mesosphere/cassandra/ConfigServer.scala
@@ -24,31 +24,39 @@ class ConfigServer(port: Int, cassConfigDir: String, seedNodes: mutable.Set[Stri
 
       info(s"Serving config resource: ${target}")
 
-      val plainFilename = new File(new URI(target).getPath).getName // don't ask...
-      val confFile = new File(s"${cassConfigDir}/${plainFilename}")
+      val requestPath = new URI(target).getPath
 
-      if (!confFile.exists()) {
-        throw new FileNotFoundException(s"Couldn't file config file: ${cassConfigDir}/${plainFilename}. Please make sure it exists.")
+      // if it's "/" - send HTTP 200
+      if(requestPath == "/") {
+        response.setStatus(HttpServletResponse.SC_OK)
+        baseRequest.setHandled(true)
+      } else {
+        val plainFilename = new File(requestPath).getName // don't ask...
+        val confFile = new File(s"${cassConfigDir}/${plainFilename}")
+
+        if (!confFile.exists()) {
+          throw new FileNotFoundException(s"Couldn't file config file: ${cassConfigDir}/${plainFilename}. Please make sure it exists.")
+        }
+
+        val fileContent = Source.fromFile(confFile).getLines()
+
+        val substitutedContent = fileContent.map {
+          line =>
+            line.replaceAllLiterally("${seedNodes}", seedNodes
+              .mkString(","))
+              .replaceAllLiterally("${clusterName}", clusterNameSlugged
+              .mkString(","))
+        }.mkString("\n")
+
+        response.setContentType("application/octet-stream;charset=utf-8")
+        response.setHeader("Content-Disposition", s"""attachment; filename="${plainFilename}" """)
+        response.setHeader("Content-Transfer-Encoding", "binary")
+        response.setHeader("Content-Length", substitutedContent.length.toString)
+
+        response.setStatus(HttpServletResponse.SC_OK)
+        baseRequest.setHandled(true)
+        response.getWriter().println(substitutedContent)
       }
-
-      val fileContent = Source.fromFile(confFile).getLines()
-
-      val substitutedContent = fileContent.map {
-        line =>
-          line.replaceAllLiterally("${seedNodes}", seedNodes
-            .mkString(","))
-            .replaceAllLiterally("${clusterName}", clusterNameSlugged
-            .mkString(","))
-      }.mkString("\n")
-
-      response.setContentType("application/octet-stream;charset=utf-8")
-      response.setHeader("Content-Disposition", s"""attachment; filename="${plainFilename}" """)
-      response.setHeader("Content-Transfer-Encoding", "binary")
-      response.setHeader("Content-Length", substitutedContent.length.toString)
-
-      response.setStatus(HttpServletResponse.SC_OK)
-      baseRequest.setHandled(true)
-      response.getWriter().println(substitutedContent)
     }
 
   }
