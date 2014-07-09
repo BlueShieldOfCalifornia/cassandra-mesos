e6f23613c006c96b171a8a5f2d9305869fedf0e0
Yulian Slobodyan <y.slobodian@gmail.com>
2014-07-08 20:03:25 -0500

    bash param evaluation needs "${param_num}" if param_num>9

--- bin/run_cassandra.sh
+++ bin/run_cassandra.sh
@@ -14,7 +14,7 @@ sed -e "s/<ZK_HOSTS>/$1/" \
     -e "s/<CONF_SERVER_INTERNAL_PORT>/$8/" \
     ./cassandra-mesos-2.0.9-2/conf/mesos.yaml.tmpl > ./cassandra-mesos-2.0.9-2/conf/mesos.yaml
 
-sed -e "s/<CLUSTER_NAME>/$9/" -e "s/<WORK_DIR>/$10/" ./cassandra-mesos-2.0.9-2/conf/cassandra.yaml.tmpl > ./cassandra-mesos-2.0.9-2/conf/cassandra.yaml
+sed -e "s/<CLUSTER_NAME>/$9/" -e "s/<WORK_DIR>/${10}/" ./cassandra-mesos-2.0.9-2/conf/cassandra.yaml.tmpl > ./cassandra-mesos-2.0.9-2/conf/cassandra.yaml
 
 cd cassandra-mesos-2.0.9-2
 ./bin/cassandra-mesos
